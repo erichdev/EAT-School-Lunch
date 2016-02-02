eeb019a90cbf73f9d9a425059961dbb4c0044fff
Erich Abellera <a.erich@gmail.com>
2016-02-02 12:32:04 -0800

    Create .net app

---
+++ EATLunchApp/EATLunchApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EATLunchApp.MvcApplication" Language="C#" %>
---
+++ EATLunchApp/EATLunchApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EATLunchApp.Startup))]
+namespace EATLunchApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
